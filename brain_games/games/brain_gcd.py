2ae0003486c94a90b702f8f4d8bf090de9b6c4dc
Oleg Gudzii <oleg198679@gmail.com>
2024-02-25 12:14:25 +0300

    Add brain-gcd game

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,23 @@
+import random
+import brain_games.game_engine
+
+
+game_message = "Find the greatest common divisor of given numbers."
+
+
+def get_gcd(number1, number2):
+    return number1 if number2 == 0 else get_gcd(number2, number1 % number2)
+
+
+def get_question_and_answer():
+    question_number_1 = random.randint(1, 100)
+    question_number_2 = random.randint(1, 100)
+    game_question = f"{question_number_1} {question_number_2}"
+    right_answer = str(get_gcd(question_number_1, question_number_2))
+
+    return (game_question, right_answer)
+
+
+def play_brain_gcd():
+    brain_games.game_engine.play_game(game_message, get_question_and_answer)
+    return
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import brain_games.games.brain_gcd
+
+
+def main():
+    brain_games.games.brain_gcd.play_brain_gcd()
+    return
+
+
+if __name__ == "__main__":
+    main()
